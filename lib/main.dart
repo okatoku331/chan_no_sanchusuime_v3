8d59778ecdc5c51922913900932f7b3f0a71c6b1
okatoku331 <okatoku331@gmail.com>
2021-02-15 11:41:13 +0900

    5版　AdMobのバナー広告設置準備(2021.1.15 11:40)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Admob.initialize();
   runApp(MyApp());
 }
 
---
+++ lib/services/admob.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class AdMobService {
+  String getBannerAdUnitId() {
+    // iOSとAndroidで広告ユニットIDを分岐させる
+    if (Platform.isAndroid) {
+      // Androidの広告ユニットID
+      return 'ca-app-pub-3940256099942544/6300978111';
+    } else if (Platform.isIOS) {
+      // iOSの広告ユニットID
+      return 'ca-app-pub-3940256099942544/2934735716';
+    }
+    return null;
+  }
+
+  // 表示するバナー広告の高さを計算
+  double getHeight(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final percent = (height * 0.06).toDouble();
+
+    return percent;
+  }
+}
