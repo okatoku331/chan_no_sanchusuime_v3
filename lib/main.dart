20df96f4cc69af28809c5794c8b2d7afec2ac09a
okatoku331 <okatoku331@gmail.com>
2021-02-15 16:40:33 +0900

    6版　AdMobのバナー広告設置・main,甲(2021.1.15 16:40)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:chan_no_sanchusuimei_v3/services/admob.dart';
+import 'package:chan_no_sanchusuimei_v3/nikkan_kinoe.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,20 +43,62 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  String text = '甲';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            AdmobBanner(
+                adUnitId: AdMobService().getBannerAdUnitId(),
+                adSize: AdmobBannerSize(
+                  width: MediaQuery.of(context).size.width.toInt(),
+                  height: AdMobService().getHeight(context).toInt(),
+                  name: 'SMART_BANNER',
+                )
+            ),
             Image.asset('images/チャン_1.jpg'),
             Padding(
                 padding: const EdgeInsets.all(32.0),
               child: Text('あなたの日干はなんですか？'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 280,
+              color: Colors.white70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Container(
+                    width: 150,
+                    height: 300,
+                    color: Colors.white70,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        RaisedButton(
+                            child: Text('甲'),
+                            onPressed: () async {
+                              final result = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => NikkanKinoe(),
+                                  ));
+                              //text = result;
+                              //print(text);
+                            },
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
 
           ],
---
+++ lib/nikkan_kinoe.dart
@@ -0,0 +1,124 @@
+import 'package:admob_flutter/admob_flutter.dart';
+import 'package:chan_no_sanchusuimei_v3/services/admob.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NikkanKinoe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('日干：甲（きのえ）'),
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.white70,
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                  '　日干が甲の人は、樹木にたとえられる性質を持っています。'
+              ),
+            ),
+            ListTile(
+              title: Image.asset(
+                  'images/甲_大樹.jpg'
+              ),
+            ),
+            ListTile(
+              title: Text(
+                  '　樹木は天に向かって伸びるように、向上心にあふれ、新しいことをしようと'
+                      'する気持ちが強く、現在おかれている境遇よりもさらに成長しようと努'
+                      '力します。'
+              ),
+            ),
+            ListTile(
+              title: Text(
+                  '　樹木は大地にしっかりと根を張るように、根性があり、初志貫徹します。困難'
+                      'があっても負けずにやり抜く人です。'
+              ),
+            ),
+            ListTile(
+              title: Text(
+                  '　樹木は幹が太くまっすぐで力強さがあるように、曲がったことや筋の通らな'
+                      'いことができません。性格は嘘が嫌いです。質実剛健で、仕事ぶりは堅実'
+                      'さを信条とします。軽薄さやいい加減さはなく、責任感が強いほうです。'
+                      'うまい儲け話にのるとか、誘惑に負けるというようなことは少ないでしょ'
+                      'う。'
+              ),
+            ),
+
+            ListTile(
+              title: Image.asset(
+                  'images/甲_木陰.jpg'
+              ),
+            ),
+            ListTile(
+              title: Text(
+                  '　樹木が枝をはり、その木陰に生き物が集うように、思いやりがあり、優しく、'
+                      'かわいそうな人や困っている人に手助けをします。しかし、おせっかいが'
+                      'すぎる面もときどきあります。'
+              ),
+            ),
+            ListTile(
+              title: Text(
+                  '　生き方は道徳を重んじます。その反面、かたくなに規範を守り、妥協を知らな'
+                      'いところもあります。'
+              ),
+            ),
+
+            ListTile(
+              title: Text(
+                  ''
+              ),
+            ),
+
+
+
+            AdmobBanner(
+                adUnitId: AdMobService().getBannerAdUnitId(),
+                adSize: AdmobBannerSize(
+                  width: MediaQuery.of(context).size.width.toInt(),
+                  height: AdMobService().getHeight(context).toInt(),
+                  name: 'SMART_BANNER',
+                )
+            ),
+
+            ListTile(
+              title: Text(
+                  ''
+              ),
+            ),
+
+            RaisedButton(
+              child: Text('戻る'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+
+
+
+
+
+
+
+
+          ],
+        ),
+
+      ),
+
+      //bottomNavigationBar: BottomNavigationBar(
+      //  items: const[
+      //    BottomNavigationBarItem(
+      //      icon: Icon(Icons.home),
+      //      label: '',
+      //    )
+      //  ],
+      //),
+
+    );
+  }
+
+}
