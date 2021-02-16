11a293b81a58c2446996bfc4b87d33920b705afc
okatoku331 <okatoku331@gmail.com>
2021-02-16 14:24:55 +0900

    11版　AdMobのIDを本アプリ用に変更(2021.1.16 14:00)

--- lib/services/admob.dart
+++ lib/services/admob.dart
@@ -6,10 +6,10 @@ class AdMobService {
     // iOSとAndroidで広告ユニットIDを分岐させる
     if (Platform.isAndroid) {
       // Androidの広告ユニットID
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-6197150911254354/6910840748';
     } else if (Platform.isIOS) {
       // iOSの広告ユニットID
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-6197150911254354/8637488407';
     }
     return null;
   }
