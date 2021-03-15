c1a84e618f95730893dd81491ed6d6d3746f0f5d
okatoku331 <okatoku331@gmail.com>
2021-03-16 06:35:37 +0900

    16.10版　生年月日の結果画面を追加,flutter upgrate に伴い変更、admobインタ広告を削除した。正常動作(2021.3.16 06:32)

--- lib/services/admob.dart
+++ lib/services/admob.dart
@@ -6,9 +6,13 @@ class AdMobService {
     // iOSとAndroidで広告ユニットIDを分岐させる
     if (Platform.isAndroid) {
       // Androidの広告ユニットID
+      // 　　　ca-app-pub-3940256099942544/6300978111　test用 広告ユニットID：
+      //      ca-app-pub-6197150911254354/6910840748  本番用　広告ユニットID：
       return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       // iOSの広告ユニットID
+      //      ca-app-pub-3940256099942544/2934735716　test用 広告ユニットID：
+      //      ca-app-pub-6197150911254354/8637488407  本番用　広告ユニットID：
       return 'ca-app-pub-6197150911254354/8637488407';
     }
     return null;
