eb9b5e1816d874479bce415404a6ef0154f7d651
okatoku331 <okatoku331@gmail.com>
2021-04-13 21:33:41 +0900

    第23.9版　今日の運勢画面LineView形式に変更(2021.4.13 21:33)

--- lib/kyou_unsei.dart
+++ lib/kyou_unsei.dart
@@ -30,16 +30,7 @@ class KyouUnsei extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //String tenunMeiMoji0 = '干支併臨';
-    //String tenunMeiMoji1 = '干支併臨';
-    //String tenunMeiMoji2 = '干支併臨';
-    //String tenunMeiMoji3 = '干支併臨';
-    //String tenunMeiMoji4 = '干支併臨';
-    //String tenunText0;
-    //String tenunText1;
-    //String tenunText2;
-    //String tenunText3;
-    //String tenunText4;
+    //　変数定義
 
     //■■画面生成のための各種データを生成する■■
 
@@ -66,55 +57,16 @@ class KyouUnsei extends StatelessWidget {
       appBar: AppBar(
         title: Text('今日の運勢'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset('images/hana_sakura.jpg'),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text('$titleSeinengappi 生まれの'),
-                Text('あなたの今日の運勢は、KyouUnnseiです。'),
-              ],
-            ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('　今日の運勢は・・・'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text('天運の年は、'),
-                Text(''),
-                Text('tenunText1'),
-                Text('tenunText2'),
-                Text('tenunText3'),
-                Text('tenunText4'),
-              ],
-            ),
+          ListTile(
+            title: Text(' '),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SizedBox(
-              width: 90,
-              height: 36,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  elevation: 4,
-                  shadowColor: Colors.red,
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text('戻る'),
-              ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 8, //　縦幅の低い端末はこの値を下げる
-            color: Colors.white70,
-            child: Text(''),
+          ListTile(
+            title: Text(' '),
           ),
           AdmobBanner(
               adUnitId: AdMobService().getBannerAdUnitId(),
@@ -123,176 +75,24 @@ class KyouUnsei extends StatelessWidget {
                 height: AdMobService().getHeight(context).toInt(),
                 name: 'SMART_BANNER',
               )),
+          ListTile(
+            title: Text(''),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SizedBox(
+              width: 90,
+              height: 36,
+              child: ElevatedButton(
+                child: Text('戻る'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-// 関数定義　文字列リストから検索文字列が先頭から何文字目にあるか返す
-//  c = nanmojime(a,b)
-//  a:　120文字（2文字×60組）の文字列リスト
-//  b:　2文字の検索文字
-//  c:　-2：一致が2組以上ある
-//      -1:一致する組がない
-//      0 ～ 59 : 組めに1組だけある
-int nanmojime(String mojilist, String kensaku2moji) {
-  int nanbanme = -1;
-  int ittisuu = 0;
-  for (int i = 0; i < 60; i++) {
-    String nimoji = mojilist.substring(i * 2, i * 2 + 2);
-    if (nimoji == kensaku2moji) {
-      nanbanme = i;
-      ittisuu = ittisuu + 1;
-    } else {}
-  }
-  if (ittisuu > 1) {
-    nanbanme = -2;
-  } else if (ittisuu < 1) {
-    nanbanme = -1;
-  } else {}
-  return nanbanme;
-}
-
-// 関数定義　干支から対応する西暦年と年齢を99歳までの間で算出する
-//   c = kannsiHeirin(a,b)
-//   a: 干支No.を表す0～59の数字　（　0:甲子　1:乙丑　～　58:壬戌　59:癸亥　）
-//   b: 生年月日を表す文字列　（yyyy-MM-dd）
-//   c: 干支併臨の年を表す 数字の配列（　[0,0,1961,2021,0] )
-//
-List kansiHeirin(int kansiSuu, String seinengappiMojiretu) {
-  List kansiHeirinList = [];
-  // 生年月日（文字列）を　（DateTime型）に変換する
-  DateTime date3 = DateTime.parse(seinengappiMojiretu); // StringからDate
-  int seinen = int.parse(seinengappiMojiretu.substring(0, 4));
-  DateTime date4 = DateTime(seinen, 2, 4);
-  //　1900.1.1 (甲辰）から誕生日までの日数を算出する
-  var nissuu = date3.difference(date4).inDays;
-  if (nissuu < 0) {
-    --seinen;
-  }
-  int nen = 0;
-  for (int i = 0; i < 6; ++i) {
-    nen = 1864 + 60 * i + kansiSuu;
-    if (nen <= seinen) {
-      kansiHeirinList.add(0);
-    } else if (nen > seinen + 100) {
-      kansiHeirinList.add(0);
-    } else {
-      kansiHeirinList.add(nen);
-    }
-  }
-  return kansiHeirinList;
-}
-
-// 関数定義　生年月日から　年柱の六十干支を算出する
-//  b = nentyuu(a)
-//   a: 生年月日を表す文字列　（yyyy-MM-dd）
-//   b: 年柱の六十干支を表す数字　（0～59）（例　0:　甲子）
-//
-int nentyuu(String a) {
-  // 生年月日（文字列）を　（DateTime型）に変換する
-  DateTime date3 = DateTime.parse(a); // StringからDate
-  int seinen = int.parse(a.substring(0, 4));
-  DateTime date4 = DateTime(seinen, 2, 4);
-  //　1900.1.1 (甲辰）から誕生日までの日数を算出する
-  var nissuu = date3.difference(date4).inDays;
-  if (nissuu < 0) {
-    --seinen;
-  }
-  var kansiSui = (seinen - 1924) % 60;
-  return kansiSui;
-}
-
-// 関数定義　天運到来の西暦とその時を迎える満年齢と種類を西暦昇順に並べる配列を生成する
-//  f = tenun(a,b,c,d,e)
-//  a: 干支併臨（年）の年を表す 数字の配列（　[0,0,0,2017,0] )
-//  b: 干支併臨（月）の年を表す 数字の配列（　[0,0,0,0,0] )
-//  c: 干支併臨（日）の年を表す 数字の配列（　[0,0,1962,2022,0] )
-//  d: 天地徳合　　　の年を表す 数字の配列（　[0,0,0,2007,0] )
-//  e: 生年月日を表す文字列　（yyyy-MM-dd）
-//　f: 天運到来の年yyyy立春時の満年齢GG種類nを表す数字配列
-//              [yyyyGGn]  n: 1: 干支併臨（年柱）
-//                            2: 干支併臨 (月柱）
-//                            3: 干支併臨 (日柱）
-//                            4: 天地徳合
-//          例　[1962053,1963062,2007504,2017601,2022653,2023662]
-//
-List tenun(List a, List b, List c, List d, String e) {
-  List<int> tenunNen = [];
-  int nen = 0;
-  // 生年月日（文字列）を　（DateTime型）に変換する
-  DateTime date3 = DateTime.parse(e); // StringからDate
-  int tenunMei;
-  List nenList = [];
-  // 年柱
-  nenList = a;
-  tenunMei = 1;
-  for (int i = 0; i < 6; ++i) {
-    nen = nenList.removeAt(0);
-    if (nen != 0) {
-      DateTime date4 = DateTime(nen, 2, 4);
-      var nissuu = date4.difference(date3).inDays;
-      print('nissuu:$nissuu');
-      var nenrei = nissuu ~/ 365.2422;
-      var tenunDate = nen * 1000 + nenrei * 10 + tenunMei;
-      tenunNen.add(tenunDate);
-    } else {}
-  }
-  // 月柱
-  nenList = b;
-  tenunMei = 2;
-  for (int i = 0; i < 6; ++i) {
-    nen = nenList.removeAt(0);
-    if (nen != 0) {
-      DateTime date4 = DateTime(nen, 2, 4);
-      var nissuu = date4.difference(date3).inDays;
-      var nenrei = nissuu ~/ 365.2422;
-      var tenunDate = nen * 1000 + nenrei * 10 + tenunMei;
-      tenunNen.add(tenunDate);
-    } else {}
-  }
-  // 日柱
-  nenList = c;
-  tenunMei = 3;
-  for (int i = 0; i < 6; ++i) {
-    nen = nenList.removeAt(0);
-    if (nen != 0) {
-      DateTime date4 = DateTime(nen, 2, 4);
-      var nissuu = date4.difference(date3).inDays;
-      var nenrei = nissuu ~/ 365.2422;
-      var tenunDate = nen * 1000 + nenrei * 10 + tenunMei;
-      tenunNen.add(tenunDate);
-    } else {}
-  }
-  // 天地徳合
-  nenList = d;
-  tenunMei = 4;
-  for (int i = 0; i < 6; ++i) {
-    nen = nenList.removeAt(0);
-    if (nen != 0) {
-      DateTime date4 = DateTime(nen, 2, 4);
-      var nissuu = date4.difference(date3).inDays;
-      var nenrei = nissuu ~/ 365.2422;
-      var tenunDate = nen * 1000 + nenrei * 10 + tenunMei;
-      tenunNen.add(tenunDate);
-    } else {}
-  }
-  // 西暦昇順にソートする
-  tenunNen.sort((num1, num2) => num1 - num2);
-  return tenunNen;
-}
-
-// 六十干支リストを生成する　（String rokujukkansi　の文字列をつくるため）
-//String rokujuuKansi = '';
-//for (int i = 0; i < 60; i++) {
-//  var j = i % 10;
-//  var k = i % 12;
-//  var kan = jukkan.substring(j, j + 1);
-//  var si = juunisi.substring(k, k + 1);
-//  rokujuuKansi = rokujuuKansi + kan + si;
-//  print('$i');
-//  print('$j');
-//  print('$k');
-//  print('$rokujuuKansi');
-//};
