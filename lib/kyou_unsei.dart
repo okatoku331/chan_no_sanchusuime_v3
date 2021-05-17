6a7e98e59e910529a23b0c278c4127b5d40c57ed
okatoku331 <okatoku331@gmail.com>
2021-05-17 22:02:14 +0900

    第26.12版　「今日の運勢」支合・三合・冲害刑を追加(2021.5/17 22:01)

--- lib/kyou_unsei.dart
+++ lib/kyou_unsei.dart
@@ -18,9 +18,12 @@ class KyouUnsei extends StatelessWidget {
   final String jukkan = "甲乙丙丁戊己庚辛壬癸"; //十干リスト
   final String jukkanYomi = "甲【きのえ】　乙【きのと】　丙【ひのえ】　丁【ひのと】　戊【つちのえ】"
       "己【つちのと】庚【かのえ】　辛【かのと】　壬【みずのえ】癸【みずのと】"; //十干リスト【よみ】
-  final String gogyou = "木＋木−火＋火−土＋土−金＋金−水＋水−";// 五行リスト
+  final String gogyou = "木＋木ー火＋火ー土＋土ー金＋金ー水＋水ー";// 五行リスト
   final String juunisi = "子丑寅卯辰巳午未申酉戌亥"; //十二支リスト
-  final String rokujukkansi = "甲子乙丑丙寅丁卯戊辰己巳庚午辛未壬申癸酉"
+  final String juunisiMoji = "0123456789ab"; //十二支数リスト
+  final String juunisiYomi = "ね　　うし　とら　う　　たつ　み　　うま　ひつじさる　とり　いぬ　い　　";
+  final String rokujukkansi =
+      "甲子乙丑丙寅丁卯戊辰己巳庚午辛未壬申癸酉"
       "甲戌乙亥丙子丁丑戊寅己卯庚辰辛巳壬午癸未"
       "甲申乙酉丙戌丁亥戊子己丑庚寅辛卯壬辰癸巳"
       "甲午乙未丙申丁酉戊戌己亥庚子辛丑壬寅癸卯"
@@ -62,7 +65,21 @@ class KyouUnsei extends StatelessWidget {
       "　　合　　　　　　　"
       "　　　合　　　　　　"
       "　　　　合　　　　　";
-  final String sigou = "　合　　三　冲害三　　　"
+  /*final String sigouZu =
+      "000102030405060708090a0b"
+      "101112131415161718191a1b"
+      "202122232425262728292a2b"
+      "303132333435363738393a3b"
+      "404142434445464748494a4b"
+      "505152535455565758595a5b"
+      "606162636465666768696a6b"
+      "707172737475767778797a7b"
+      "808182838485868788898a8b"
+      "909192939495969798999a9b"
+      "a0a1a2a3a4a5a6a7a8a9aaab"
+      "b0b1b2b3b4b5b6b7b8b9babb";*/
+  /*final String sigou =
+      "　合　　三　冲害三　　　"
       "合　　　　三害冲　三　　"
       "　　　　　害三　冲　三合"
       "　　　　害　　三　冲合三"
@@ -73,19 +90,33 @@ class KyouUnsei extends StatelessWidget {
       "三　冲　三合　　　　　害"
       "　三　冲合三　　　　害　"
       "　　三合冲　三　　害　　"
+      "　　合三　冲　三害　　　";*/
+  final String sigouKei =
+      "　合　刑三　冲害三　　　"
+      "合　　　　三害一　三刑　"
+      "　　　　　二三　一　三合"
+      "刑　　　五　　三　冲合三"
+      "三　　五　　　　三合冲　"
+      "　三二　　　　　四三　冲"
+      "冲害三　　　　合　　三　"
+      "害一　三　　合　　　刑三"
+      "三　一　三四　　　　　害"
+      "　三　冲合三　　　　害　"
+      "　刑三合冲　三刑　害　　"
       "　　合三　冲　三害　　　";
-  final String sikei = "　　　刑　　　　　　　　"
+  /*final String sikei =
+      "　　　刑　　　　　　　　"
       "　　　　　　　刑　　刑　"
       "　　　　　刑　　刑　　　"
       "刑　　　　　　　　　　　"
-      "　　　　刑　　　　　　　"
+      "　　　　　　　　　　　　"
       "　　刑　　　　　刑　　　"
-      "　　　　　　刑　　　　　"
+      "　　　　　　　　　　　　"
       "　刑　　　　　　　　刑　"
       "　　刑　　刑　　　　　　"
-      "　　　　　　　　　刑　　"
+      "　　　　　　　　　　　　"
       "　刑　　　　　刑　　　　"
-      "　　　　　　　　　　　刑";
+      "　　　　　　　　　　　　";*/
   final String jukkanName = null;
   final List nitikansiHeirin = [0, 0, 0, 0, 0, 0];
   final List kansiHeirinList = [0, 0, 0, 0, 0, 0];
@@ -95,7 +126,14 @@ class KyouUnsei extends StatelessWidget {
     //　変数定義
     String kangouMoji = '';
     String sigouMoji = '';
-    String sikeiMoji = '';
+    //String sikeiMoji = '';
+    String sigouKaisetu = '';
+    String tuuhenbosiNowKaisetu = '';
+    String nikkanInYou = '';
+    int kitiPoint = 0;
+    int kitiPointNikann = 0;
+    int kitiPointNissi = 0;
+    int kitiPointKangou = 0;
 
     //■■画面生成のための各種データを生成する■■
 
@@ -128,6 +166,13 @@ class KyouUnsei extends StatelessWidget {
     //　五行を算出する
     var gogyouMoji = gogyou.substring(
         nikkan * 2, nikkan * 2 + 2);
+    var nikkanYouIn = gogyou.substring(
+        nikkan * 2 + 1, nikkan * 2 + 2);
+    if (nikkanYouIn == '＋'){
+      nikkanInYou = 'ー';
+    }else{
+      nikkanInYou = '＋';
+    }
 
 
     //　今日の通変星を算出する
@@ -144,38 +189,156 @@ class KyouUnsei extends StatelessWidget {
     var tuuhenbosiYomiNow = tuuhenbosiYomi.substring(
         tuuhenbosiSuu * 5,  tuuhenbosiSuu * 5 + 5).trimRight();
 
+    //　通変星の解説文作成
+    if (tuuhenbosiNow == '比肩') {
+      tuuhenbosiNowKaisetu = '比肩　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+    } else if (tuuhenbosiNow == '劫敗') {
+      tuuhenbosiNowKaisetu = '劫敗　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 5;
+    } else if (tuuhenbosiNow == '食神') {
+      tuuhenbosiNowKaisetu = '食神　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+    } else if (tuuhenbosiNow == '傷官') {
+      tuuhenbosiNowKaisetu = '傷官　の持つ意味は・・・'
+          '';
+      kitiPointNikann = -10;
+    } else if (tuuhenbosiNow == '偏財') {
+      tuuhenbosiNowKaisetu = '偏財　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+    } else if (tuuhenbosiNow == '正財') {
+      tuuhenbosiNowKaisetu = '正財　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+    } else if (tuuhenbosiNow == '偏官') {
+      tuuhenbosiNowKaisetu = '偏官　の持つ意味は・・・'
+          '';
+      kitiPointNikann = -10;
+    } else if (tuuhenbosiNow == '正官') {
+      tuuhenbosiNowKaisetu = '正官　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+    } else if (tuuhenbosiNow == '倒食') {
+      tuuhenbosiNowKaisetu = '倒食　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+    } else if (tuuhenbosiNow == '印綬') {
+      tuuhenbosiNowKaisetu = '印綬　の持つ意味は・・・'
+          '';
+      kitiPointNikann = 10;
+
+    }
+
+
+
+
+
     //　干合を算出する
     var kangouNow = kangou.substring(
         nikkan * 10 + nikkanNow * 1, nikkan * 10 + nikkanNow * 1 + 1);
     if (kangouNow == '合') {
-      kangouMoji = '干合があります';
+      kangouMoji = '干合(かんごう）があります。干合は、男（＋）と女（ー）の発想の違うカップルが'
+          '一緒になると新しいエネルギーが生じ、さらに変化したエネルギーも生じるという意味があります。'
+          '吉ポイントは、プラス10です。';
+      kitiPointKangou = 10;
+
     } else {}
 
-    //　日支を算出する
+    //　今日の日支を算出する
     var nissiNow = (nissuuNow + 10) % 12;
     var nissiNowMoji = juunisi.substring(nissiNow, nissiNow + 1);
+    var juunisiaNow = juunisiMoji.substring(nissiNow, nissiNow + 1);
+
     //　日支を算出する
     var nissi = (nissuu + 10) % 12;
     var nissiMoji = juunisi.substring(nissi, nissi + 1);
+    var juunisia = juunisiMoji.substring(nissi, nissi + 1);
+
+    //  支合・支冲等関係図を算出する
+
     //　支合を算出する
-    var sigouNow = sigou.substring(
+    var sigouNow = sigouKei.substring(
         nissi * 12 + nissiNow * 1, nissi * 12 + nissiNow * 1 + 1);
     if (sigouNow == '合') {
-      sigouMoji = '支合があります';
+      sigouMoji = '支合（しごう）があります';
+      sigouKaisetu = '支合の意味は、仲のいい恋人夫婦の大きな気が発生します。'
+          '大吉です。吉ポイントは、プラス20です。';
+      kitiPointNissi = 20;
     } else if (sigouNow == '三') {
-      sigouMoji = '三合があります';
+      sigouMoji = '三合（さんごう）があります';
+      sigouKaisetu = '三合の意味は、女子学生三人組のような関係で、'
+          '一人一人は弱くても三人そろうと大きな気を発生します。二人そろった半会（はんかい）でもそれなりの気を発生します。'
+          '吉です。吉ポイントは、プラス10です。';
+      kitiPointNissi = 10;
     } else if (sigouNow == '冲') {
-      sigouMoji = '冲があります';
+      sigouMoji = '冲（ちゅう）があります';
+      sigouKaisetu = '冲は、同性同士の殴り合いを意味します。'
+          '同性同士なので、激しく殴り合ったり、ひっかきあったり、気をうばわれます。'
+          '大凶です。吉ポイントは、マイナス20です。';
+      kitiPointNissi = -20;
     } else if (sigouNow == '害') {
-      sigouMoji = '害があります';
-    } else {}
+      sigouMoji = '害（がい）があります';
+      sigouKaisetu = '害は、トラブルを意味します。冲（ちゅう）に比べれば軽く、激しい喧嘩しませんが、トラブルなので気はやはりうばわれます。'
+          '凶です。吉ポイントは、マイナス10です。';
+      kitiPointNissi = -10;
+
+    } else if (sigouNow == '刑') {
+      sigouMoji = '刑（けい）があります';
+      sigouKaisetu = '刑は、トラブルを意味します。冲（ちゅう）に比べれば軽く、激しい喧嘩しませんが、トラブルなのでやはり気はうばわれます。'
+          '凶です。吉ポイントは、マイナス10です。';
+      kitiPointNissi = -10;
+
+    } else if (sigouNow == '一') {
+      sigouMoji = '冲（ちゅう）と刑（けい）があります';
+      sigouKaisetu = '悪い作用が重なるのではなく、あるときは冲になり、あるときは刑になります。冲は、同性同士の殴り合いを意味します。'
+          '刑は冲に比べれば軽く、激しい喧嘩しませんが、トラブルなのでやはり気はうばわれます。'
+          '凶です。吉ポイントは、マイナス15です。　';
+      kitiPointNissi = -15;
+
+
+    } else if (sigouNow == '二') {
+      sigouMoji = '害（がい）と刑（けい）があります';
+      sigouKaisetu = '悪い作用が重なるのではなく、あるときは害になり、あるときは刑になります。'
+          '刑は重いトラブル、害は軽いトラブルを意味します。トラブルなのでやはり気はうばわれます。'
+          '凶です。吉ポイントは、マイナス10です。　';
+      kitiPointNissi = -10;
+
+
+    } else if (sigouNow == '四') {
+      sigouMoji = '支合（しごう）と刑（けい）があります';
+      sigouKaisetu = '良い作用の支合と悪い作用の刑が重なります。普段は仲のいい夫婦が急に大げんかするようなような意味です。'
+          '仲がいいときは気を発生しますが、喧嘩して気がうばわれます。'
+          '吉です。吉ポイントは、プラス10です。　';
+      kitiPointNissi = 10;
+
+
+    } else if (sigouNow == '五') {
+      sigouMoji = '卯辰の害があります';
+      sigouKaisetu = '害（がい）は、トラブルを意味しますが、卯辰の害は、冲（ちゅう）のなぐりあいに匹敵する喧嘩を意味します。'
+          '夫婦の激しい喧嘩の意味です。気は激しくうばわれます。'
+          '中凶です。吉ポイントは、マイナス15です。';
+      kitiPointNissi = -15;
+
+    } else {
+      sigouMoji = '支合・三合・冲・害・刑はありません';
+      sigouKaisetu = '吉ポイントは、プラスマイナス０です。';
+      kitiPointNissi = 0;
+
+    }
 
     //　支刑を算出する
-    var sikeiNow = sikei.substring(
-        nissi * 12 + nissiNow * 1, nissi * 12 + nissiNow * 1 + 1);
-    if (sikeiNow == '刑') {
-      sigouMoji = '刑があります';
-    } else {}
+    //var sikeiNow = sikei.substring(
+    //    nissi * 12 + nissiNow * 1, nissi * 12 + nissiNow * 1 + 1);
+    //if (sikeiNow == '刑') {
+    //  sigouMoji2 = '・刑';
+    //} else {}
+
+    kitiPoint = kitiPointNikann + kitiPointNissi + kitiPointKangou;
+
 
     //■■　画面を生成する　■■
 
@@ -191,38 +354,52 @@ class KyouUnsei extends StatelessWidget {
           ),
 
           ListTile(
-            title: Text('　　生年月日の日干が、$jukkanMoji（$gogyouMoji）、'),
+            title: Text('１．「日干」のからみた今日の運勢'),
           ),
+
+
           ListTile(
-            title: Text('　　今日の日干が、$jukkanNow（$gogyouNow）なので、'),
-          ),
-          ListTile(
-            title: Text('　今日の通変星は、$tuuhenbosiNow（$tuuhenbosiYomiNow）になります。図で表すと'
-                '下図のようになります。'),
+            title: Text('　生年月日の日干が、「$jukkanMoji（$gogyouMoji）」、'
+                '今日の日干が、「$jukkanNow（$gogyouNow）」なので、'
+                '今日の通変星（つうへんぼし：日干との関係性）は、'
+                '$tuuhenbosiNow（$tuuhenbosiYomiNow）になります。　　図で表すと'
+                '下図のようになります。薄い灰色の円のすぐ内側が（$nikkanYouIn）、'
+                'さらに内側が（$nikkanInYou）を表しています。'),
           ),
           ListTile(
             title: Image.asset('images/tuuhenbosi/$tuuhendosizuNow.jpg'),
           ),
           ListTile(
-            title: Text('　$tuuhenbosiNow　の持つ意味は、エネルギーを授け受けして運勢が'
-                '強くなります。自分が強くなりすぎて、周りの人を傷つけてしまうこともありますので注意しましょう。'
-                '　開運方法は、他の人に尽くすことです。'
-                ),
+            title: Text('　$tuuhenbosiNowKaisetu'),
           ),
           ListTile(
             title: Text('　　　　　$kangouMoji'),
           ),
+
           ListTile(
-            title: Text('　　　　　$nissi：$nissiMoji：生年月日の日支 　'),
+            title: Text('２．「日支」のからみた今日の運勢'),
           ),
           ListTile(
-            title: Text('　　　　　$nissiNow：$nissiNowMoji：今日の日干　'),
+            title: Text('　　生年月日の日支が、「$nissiMoji」、'
+                '今日の日支が、「$nissiNowMoji」で、その関係性を表したのが、下図になります。'
+                'ピンクの矢印がさす部分が今日の運勢です。'),
           ),
           ListTile(
-            title: Text('　　　　　$sigouMoji　'),
+            title: Image.asset('images/sigou/s$juunisia$juunisiaNow.jpg'),
+          ),
+
+          ListTile(
+            title: Text('　$sigouMoji。$sigouKaisetu　'),
           ),
           ListTile(
-            title: Text('　　　　　$sikeiMoji　　'),
+            title: Text('　'),
+          ),
+
+          ListTile(
+            title: Text('３．総合ポイント'),
+          ),
+          ListTile(
+            title: Text('　本日の吉凶ポイント：　$kitiPoint'),
           ),
           ListTile(
             title: Text('　'),
