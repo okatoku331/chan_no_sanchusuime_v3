f41798aabb4340bc35518be37ad8c5355671d99e
okatoku331 <okatoku331@gmail.com>
2021-07-14 21:38:17 +0900

    第36.12版　プロバイダーパターン8/add_book_model/変更、(2021.7.14 21:38)

--- lib/book_list/add_book/add_book_model.dart
+++ lib/book_list/add_book/add_book_model.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../book.dart';
+
 class AddBookModel extends ChangeNotifier {
   String bookTitle = '';
 
@@ -11,6 +13,15 @@ class AddBookModel extends ChangeNotifier {
     FirebaseFirestore.instance.collection('books').add({
       'title': bookTitle,
       'createdAT': Timestamp.now(),
+    });
+  }
+
+  Future updateBook(Book book) async {
+    final document =
+        FirebaseFirestore.instance.collection('books').doc(book.documentID);
+    await document.update({
+      'title': bookTitle,
+      'updatedAT': Timestamp.now(),
     });
   }
 }
